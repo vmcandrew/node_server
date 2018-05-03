26468f8412ef36f547b7eac5106ab8225ccc83ed
Vlad McAndrew <vladislav.mcandrew@gmail.com>
2018-05-03 17:17:27 +0000

    router setup implemented in script

--- server.js
+++ server.js
@@ -5,62 +5,26 @@ let fs = require('fs');
 let express = require('express');
 
 const app = express();
-app.use(express.static(path.join(__dirname,'public')));
+let router = express.Router();
 
-// extensions = {
-// 	".html" : "text/html",
-// 	".css" : "text/css",
-// 	".js" : "application/javascript",
-// 	".png" : "image/png",
-// 	".gif" : "image/gif",
-// 	".jpg" : "image/jpeg"
-// };
-
-// function getFile(filePath,res,page404,mimeType){
-// 	fs.exists(filePath,function(exists){
-// 		if(exists){
-// 			fs.readFile(filePath,function(err,contents){
-// 				if(!err){
-// 					res.writeHead(200,{
-// 						"Content-type" : mimeType,
-// 						"Content-Length" : contents.length
-// 					});
-// 					res.end(contents);
-// 				} else {
-// 					console.dir(err);
-// 				};
-// 			});
-// 		} else {
-
-// 			fs.readFile(page404,function(err,contents){
-// 				if(!err){
-// 					res.writeHead(404, {'Content-Type': 'text/html'});
-// 					res.end(contents);
-// 				} else {
-// 					console.dir(err);
-// 				};
-// 			});
-// 		};
-// 	});
-// };
-
-
-// function requestHandler(req, res) {
-// 	 let fileName = path.basename(req.url) || 'index.html'
-// 	 let localFolder = __dirname + '/public/';
-// 	 let page404 = localFolder + '404.html';
-// 	 let ext = path.extname(fileName)
-
-// 	 if(!extensions[ext]){
-// 	 	res.writeHead(404, {'Content-Type': 'text/html'});
-// 	 			res.end("&lt;html&gt;&lt;head&gt;&lt;/head&gt;&lt;body&gt;The requested file type is not supported&lt;/body&gt;&lt;/html&gt;");
-// 	};
-
-// 	 getFile((localFolder + fileName),res,page404,extensions[ext]);
-// };
-
-app.get('/', function(req, res){
-	res.sendfile('index.html');
+router.use(function (req,res,next) {
+	console.log("/" + req.method);
 });
-app.listen(8080);
+
+router.get("/",function(req,res){
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+router.get("/about",function(req,res){
+  res.sendFile(__dirname + "/public/about.html");
+});
+
+app.use("/",router);
+
+app.use("*",function(req,res){
+	res.sendFile(__dirname + "/public/404.html");
+});
+app.listen(8080,function(){
+	console.log("Live at port 8080");
+});
 //http.createServer(requestHandler).listen(8080);
