27fe4c073b4fa0da19ed0fc368a710e7b1b18f8b
Vlad McAndrew <vladislav.mcandrew@gmail.com>
2018-05-03 14:27:41 +0000

    created node_server repository and added basic node and html files

---
+++ server.js
@@ -0,0 +1,11 @@
+let http = require('http');
+let url = require('url');
+let fs = require('fs');
+
+http.createServer(function (req, res) {
+	fs.readFile('index.html', function(err, data) {
+		res.writeHead(200, {'Content-Type': 'text/html'});
+		res.write(data);
+		res.end();
+	});
+}).listen(8080);
