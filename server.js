dfa58b3a1a7c3c3aef901e9a60bd99253a954eb3
Vlad Mcandrew <vladislav.mcandrew@gmail.com>
2018-05-04 11:27:37 -0400

    added comments to server.js

--- server.js
+++ server.js
@@ -2,12 +2,13 @@ let http = require('http');
 let url = require('url');
 let path = require('path');
 let fs = require('fs');
-let express = require('express');
+let express = require('express'); //this is express 4.0
 
-let t1="test";
+let t1="test"; //test data recieved via GET request
 
 const app = express();
 app.use(express.static(path.join(__dirname,'public')));
+//can probably be removed
 
 extensions = [{
 	".html" : "text/html",
@@ -17,22 +18,30 @@ extensions = [{
 	".gif" : "image/gif",
 	".jpg" : "image/jpeg"
 }];
-
+//extensions for encoding
 
 app.get('/', function(req, res){
 	res.sendfile('index.html');
 });
+//serves home page
 
 app.get('/about', function(req, res){
 	res.sendfile(__dirname + '/public/about.html');
 });
 
+//loads about page
+
 app.get('/test',function(req,res){
 	res.send(res.json(extensions));
 });
+
+//sends json data when test sub dir is visited or called via GET request
 
 app.get('*',function(req,res){
 	res.sendfile(__dirname + '/public/404.html');
 });
 
+//404 if page doesnt exist
+
 app.listen(8080);
+//port 8080
